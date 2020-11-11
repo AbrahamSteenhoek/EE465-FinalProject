23dcdd4b8276998625812157639061b2e5c2aa21
asteenhoek <asteenhoek@rowe-electronics.com>
2020-11-10 18:54:59 -0600

    Change numerator to 33 bits

--- calculate_numerator.v
+++ calculate_numerator.v
@@ -5,7 +5,7 @@ module calculate_numerator(
     input [3:0] N,
     input [13:0] Tsum,
     input MODE,
-    output [20:0] numerator
+    output [32:0] numerator // can be up to 33 bits large
 );
 
 assign numerator = ( MODE == 0 ) ? Tsum : sigma_hat^2*N^2 + N*Tsum^2 - Tsum^2;
