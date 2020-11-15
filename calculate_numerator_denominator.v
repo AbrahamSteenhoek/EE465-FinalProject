64f740f5a01b21fb2cac8adc27e8094ac8f508c9
asteenhoek <asteenhoek@rowe-electronics.com>
2020-11-14 22:08:57 -0600

    remove need for wires in calculator for num and denom

--- calculate_numerator_denominator.v
+++ calculate_numerator_denominator.v
@@ -8,18 +8,8 @@ module calculate_numerator_denominator(
     output [32:0] numerator, // can be up to 33 bits large
     output [21:0] denominator
 );
-wire [35:0] product1;
-wire [35:0] product2;
-wire [35:0] product3;
-wire [35:0] den;
 
-assign product1 = (sigma_hat**2) * (N**2);
-assign product2 = N*Tsum_square;
-assign product3 = Tsum**2;
-
-assign den = 2 * (N**2) * sigma_hat;
-
-assign numerator = product1 + product2 - product3;
-assign denominator = den;
+assign numerator = ((sigma_hat**2) * (N**2)) + (N*Tsum_square) - (Tsum**2);
+assign denominator = 2 * (N**2) * sigma_hat;
 
 endmodule
