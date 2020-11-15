749f51254d7689b34fecb82fb43b2f3b0150dbaa
asteenhoek <asteenhoek@rowe-electronics.com>
2020-11-14 22:02:03 -0600

    Remove redundant copy of wires

--- calculate_numerator_denominator.v
+++ calculate_numerator_denominator.v
@@ -22,18 +22,4 @@ assign den = 2 * (N**2) * sigma_hat;
 assign numerator = product1 + product2 - product3;
 assign denominator = den;
 
-// wire [30:0] prod1;
-// wire [35:0] prod2;
-// wire [31:0] prod3;
-// wire [18:0] Nsqr_x_sigma;
-
-// assign Nsqr_x_sigma = N*N*sigma_hat;
-
-// assign prod1 = Nsqr_x_sigma*sigma_hat; // assign N^2*sigma_hat^2
-// assign prod2 = N*Tsum_square; // assign N*Tsum^2
-// assign prod3 = Tsum*Tsum; // assign Tsum^2
-
-// assign numerator = prod1+prod2-prod3; 
-// assign denominator = Nsqr_x_sigma; // assign N^2*sigma_hat
-
 endmodule
