d66482a0a2fba973708468187895f15352e7ea52
asteenhoek <asteenhoek@rowe-electronics.com>
2020-11-15 22:12:49 -0600

    fix built for tb

--- register_file_tb.v
+++ register_file_tb.v
@@ -1,25 +1,36 @@
+`include "register_file.v"
+
 `timescale 1ns/1ns
 
 module register_file_tb;
 
 reg reset;
-reg [11:0] ts;
+reg [11:0] tn;
 reg sample;
 reg clk;
-reg N;
 wire tsum_actual;
 wire tsum_square_actual;
 
+always #10 clk <= clk;
 
+initial
+begin
+    clk = 0;
+    reset = 1;
+    sample = 0;
+    #40
+    reset = 0;
+    sample = 1;
+
+end
 
 register_file reg_file(
-    .RESET, 
-    .TN, 
-    .SAMPLE, 
-    .CLK, 
-    .Tsum, 
-    .Tsum_square, 
-    .N
+    .RESET(reset),
+    .TN(tn),
+    .SAMPLE(sample), 
+    .CLK(clk), 
+    .Tsum(tsum_actual), 
+    .Tsum_square(tsum_square_actual)
     );
 
 endmodule
